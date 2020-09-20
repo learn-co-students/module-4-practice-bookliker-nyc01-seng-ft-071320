95882315de52f2723dff436a50271460e158bd81
redazures <jwu_nyc@yahoo.com>
2020-09-20 13:00:54 -0400

    done

---
+++ src/Components/HeaderTop.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    Container,
+    Header,
+    Menu,
+    Button,
+    List,
+    Image
+  } from "semantic-ui-react";
+
+function HeaderTop (){
+    return(
+        <Menu inverted>
+          <Menu.Item header>Bookliker</Menu.Item>
+        </Menu>
+    )
+}
+
+export default HeaderTop
